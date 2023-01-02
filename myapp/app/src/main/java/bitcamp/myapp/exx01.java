84a8a4d0e4aa94c0043c1914220a86033bc98eb2
96abab <96abab@naver.com>
2023-01-02 20:26:59 +0900

    문제집풀기

---
+++ myapp/app/src/main/java/bitcamp/myapp/exx01.java
@@ -0,0 +1,25 @@
+package bitcamp.myapp;
+
+public class  exx01 {
+  static void charAt1() {
+    String text = "가나다라마";
+  }
+  public static void main(String[] args) {
+    String a = "AAdfg";
+    String temp = "";
+    for (int i = 0; i < a.length(); i++) {
+
+      char value = a.charAt(i);
+      //      if (value != ' ') {
+      //        int str = value;
+      //        System.out.println(str);
+      //        if ()
+      //      }
+      if (value != ' ') {
+
+      }
+
+      System.out.println(value);
+    }
+  }
+}
