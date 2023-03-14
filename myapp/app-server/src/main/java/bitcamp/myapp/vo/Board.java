46038c775c6321dea98959a104795987bf157b5d
96abab <96abab@naver.com>
2023-03-14 17:22:36 +0900

    type: Update
    React적용하기

--- myapp/app-server/src/main/java/bitcamp/myapp/vo/Board.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/vo/Board.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import lombok.Data;
 
+@Data
 public class Board implements java.io.Serializable {
   private static final long serialVersionUID = 1L;
 
@@ -40,74 +42,6 @@ public class Board implements java.io.Serializable {
       return false;
     Board other = (Board) obj;
     return no == other.no;
-  }
-
-  public int getNo() {
-    return no;
-  }
-  public void setNo(int no) {
-    this.no = no;
-  }
-  public String getTitle() {
-    return title;
-  }
-  public void setTitle(String title) {
-    this.title = title;
-  }
-  public String getContent() {
-    return content;
-  }
-  public void setContent(String content) {
-    this.content = content;
-  }
-  public String getPassword() {
-    return password;
-  }
-  public void setPassword(String password) {
-    this.password = password;
-  }
-  public Date getCreatedDate() {
-    return createdDate;
-  }
-  public void setCreatedDate(Date createdDate) {
-    this.createdDate = createdDate;
-  }
-  public int getViewCount() {
-    return viewCount;
-  }
-  public void setViewCount(int viewCount) {
-    this.viewCount = viewCount;
-  }
-  public int getWriterNo() {
-    return writerNo;
-  }
-  public void setWriterNo(int writerNo) {
-    this.writerNo = writerNo;
-  }
-  public String getWriterName() {
-    return writerName;
-  }
-  public void setWriterName(String writerName) {
-    this.writerName = writerName;
-  }
-  public Member getWriter() {
-    return writer;
-  }
-  public void setWriter(Member writer) {
-    this.writer = writer;
-  }
-  public List<BoardFile> getAttachedFiles() {
-    return attachedFiles;
-  }
-  public void setAttachedFiles(List<BoardFile> attachedFiles) {
-    this.attachedFiles = attachedFiles;
-  }
-  @Override
-  public String toString() {
-    return "Board [no=" + no + ", title=" + title + ", content=" + content + ", password="
-        + password + ", createdDate=" + createdDate + ", viewCount=" + viewCount + ", writerNo="
-        + writerNo + ", writerName=" + writerName + ", writer=" + writer + ", attachedFiles="
-        + attachedFiles + "]";
   }
 
 }
